e76b2164e9e79315b948ac4854d471493adb2ac1
obdulia <obdulia.bjimenez@gmail.com>
2020-01-24 12:24:04 -0600

    componente login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import update from 'react-addons-update'
-/*import APIInvoker from './utils/APIInvoker'*/
+import APIInvoker from '../utils/APIInvoker'
 
 import './login.css'
 class Login extends React.Component {
@@ -11,21 +11,26 @@ class Login extends React.Component {
             password: ""
         }
     }
+
     handleInput(e) {
         let field = e.target.name
         let value = e.target.value
+
+        console.log(field + '-----------'+ value );
+
         if (field === 'username') {
             value = value.replace(' ', '').replace('@', '').substring(0, 15)
             this.setState(update(this.state, {
                 [field]: { $set: value }
             }))
         }
+
         this.setState(update(this.state, {
             [field]: { $set: value }
         }))
     }
     login(e) {
-        /*e.preventDefault()
+        e.preventDefault()
         let request = {
             "username": this.state.username,
             "password": this.state.password
@@ -39,42 +44,45 @@ class Login extends React.Component {
             this.refs.submitBtnLabel.innerHTML = error.message           
             this.refs.submitBtnLabel.className = 'shake animated'           
             console.log("Error en la autenticación")           
-        })  */     
+        }) 
     }
-   
-     render() {
-        return (      
-            < div id = "signup" >             
-                < div className = "container" >                   
-                    < div className = "row" >                       
-                        < div className = "col-xs-12" >
-                        </div >           
-                    </div >           
+
+    render() {
+        return (
+            < div id="signup" >
+                < div className="container" >
+                    < div className="row" >
+                        < div className="col-xs-12" >
+                        </div >
+                    </div >
                 </div >
-                < div className = "signup-form" >
-                    < form onSubmit = { this.login.bind(this) } >
+                < div className="signup-form" >
+                    < form onSubmit={this.login.bind(this)} >
                         < h1 > Iniciar sesión en Twitter</h1 >
-                        < input type = "text" value = { this.state.username }
-                            placeholder = "usuario" name = "username" id = "username"
-                            onChange = { this.handleInput.bind(this) } />
-                        < label ref = "usernameLabel" id = "usernameLabel"
-                            htmlFor = "username" ></label > 
-                        < input type = "password" id = "passwordLabel"
-                            value = { this.state.password } placeholder = "Contraseña"
-                            name = "password" onChange = { this.handleInput.bind(this) } />
-                        < label ref = "passwordLabel" htmlFor = "passwordLabel" ></label >
-                        < button className = "btn btn-primary btn-lg " id = "submitBtn"
-                            onClick = { this.login.bind(this) } > Regístrate</button >
-                        < label ref = "submitBtnLabel" id = "submitBtnLabel" htmlFor = "submitBtn"
-                            className = "shake animated hidden " ></label >
-                        < p className = "bg-danger user-test" >
+
+                        <input type="text" value={this.state.username}
+                            placeholder="usuario" name="username" id="username"
+                            onChange={this.handleInput.bind(this)} />
+                        < label ref="usernameLabel" id="usernameLabel"
+                            htmlFor="username" ></label >
+
+                        < input type="password" id="passwordLabel"
+                            value={this.state.password} placeholder="Contraseña"
+                            name="password" onChange={this.handleInput.bind(this)} />
+                        < label ref="passwordLabel" htmlFor="passwordLabel" ></label >
+
+                        < button className="btn btn-primary btn-lg " id="submitBtn"
+                            onClick={this.login.bind(this)} > Regístrate</button >
+                        < label ref="submitBtnLabel" id="submitBtnLabel" htmlFor="submitBtn"
+                            className="shake animated hidden " ></label >
+                        < p className="bg-danger user-test" >
                             Crea un usuario o usa el usuario
                             < strong > test / test</strong ></p >
-                        < p >¿No tienes una cuenta ? Registrate</p >    
-                    </form >           
+                        < p >¿No tienes una cuenta ? Registrate</p >
+                    </form >
                 </div >
-            </div >      
+            </div >
         )
     }
 }
- export default Login
+export default Login
