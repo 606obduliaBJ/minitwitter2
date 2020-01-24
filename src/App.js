948f53d80a42b2f3fb46fef0f060c39856e45824
Araceli <araceli.edith0810@gmail.com>
2020-01-24 12:39:10 -0600

    Corrección del app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import './App.css';
 import Profile from './components/Profile';
 
@@ -11,22 +10,6 @@ function App() {
       </header>
     </div>
   );
-=======
-//import { render } from 'react-dom';
-//import logo from './logo.svg';
-import './App.css';
-//import './components/suggested_users/styles.css'
-import SuggestedUser from './components/suggested_users/SuggestedUsers'
-
-
-class App extends React.Component{
-
-  render(){
-    return(
-      <SuggestedUser/>
-    )
-  }
->>>>>>> master
 }
 
 export default App;
