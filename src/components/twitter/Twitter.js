08d212d1e4b4460e207c6f39ff4aee21785b9dd7
mire <mire12car34@gmail.com>
2020-01-30 19:00:48 -0600

    correccion detalle
    link detalle

--- src/components/twitter/Twitter.js
+++ src/components/twitter/Twitter.js
@@ -2,6 +2,7 @@
 import Profile from '../Profile'
 import React, { Component } from 'react'
 import SuggestedUser from '../suggestedUsers/SuggestedUsers'
+import {Link } from 'react-router-dom';
 
 class Twitter extends Component {
  render() {
@@ -12,7 +13,9 @@ class Twitter extends Component {
             <div>
             <Profile /> 
             <SuggestedUser/>
+            
             </div>
+            <Link to="/tweetDetail" className="link">Entrar a detalle</Link>
             
       </div>
     
